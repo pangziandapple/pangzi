ce2396959b7c251714e6b12abd5b6ac7d8f0cf1c
zhangzhiheng <zhangzhiheng@yesway.cn>
2018-01-09 13:46:54 +0800

    增加注解

--- mysqlwriteread/src/main/java/apple/pangzi/dao/UserDao.java
+++ mysqlwriteread/src/main/java/apple/pangzi/dao/UserDao.java
@@ -2,9 +2,12 @@ package apple.pangzi.dao;
 
 import org.apache.ibatis.annotations.Param;
 
+import apple.pangzi.db.aopdatasource.DataSource;
+import apple.pangzi.db.aopdatasource.DynamicDataSourceGlobal;
 import apple.pangzi.entity.User;
 
 public interface UserDao {
 
+	@DataSource(DynamicDataSourceGlobal.READ)
 	public User getUserById(@Param("id") int id);
 }
